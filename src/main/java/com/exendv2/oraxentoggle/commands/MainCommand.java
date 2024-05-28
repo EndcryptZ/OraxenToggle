1760122054a406f766bfe58e1f0221ce348ca0d4
ExEndV2 <pternej32@gmail.com>
2024-05-29 05:02:40 +0800

    Initial Commit

--- src/main/java/com/exendv2/oraxentoggle/commands/MainCommand.java
+++ src/main/java/com/exendv2/oraxentoggle/commands/MainCommand.java
@@ -1,6 +1,7 @@
 package com.exendv2.oraxentoggle.commands;
 
 import com.exendv2.oraxentoggle.OraxenToggle;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,7 +19,7 @@ public class MainCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
 
         if(!(sender instanceof Player)){
-            sender.sendMessage("This command is only available for players!");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cThis command is only available for players!"));
             return true;
         }
 
