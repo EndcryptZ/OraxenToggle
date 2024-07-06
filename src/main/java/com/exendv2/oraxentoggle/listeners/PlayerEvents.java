2bd5890ef8efa322f17d939f19b13350ed6d870b
Exnd <pternej32@gmail.com>
2024-07-06 20:10:21 +0800

    Added delay for sendPack method

--- src/main/java/com/exendv2/oraxentoggle/listeners/PlayerEvents.java
+++ src/main/java/com/exendv2/oraxentoggle/listeners/PlayerEvents.java
@@ -2,9 +2,11 @@ package com.exendv2.oraxentoggle.listeners;
 
 import com.exendv2.oraxentoggle.OraxenToggle;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerEvents implements Listener {
 
@@ -20,10 +22,28 @@ public class PlayerEvents implements Listener {
 
         boolean isEnabled = plugin.getPlayerManager().isEnabled(event.getPlayer());
         if(isEnabled){
-            String command = "/oraxen pack send";
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command + " " + event.getPlayer().getName());
+
+            asyncTaskSendPack(event.getPlayer());
 
         }
+    }
+
+    private void asyncTaskSendPack(final Player player) {
+        (new BukkitRunnable() {
+            public void run() {
+                taskSendPack(player);
+            }
+        }).runTaskLaterAsynchronously(OraxenToggle.getInstance(), 15L);
+    }
+
+    private void taskSendPack(final Player player) {
+        (new BukkitRunnable() {
+            public void run() {
+                String command = "/oraxen pack send " + player.getName();
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+            }
+
+        }).runTask(OraxenToggle.getInstance());
     }
 
 }
